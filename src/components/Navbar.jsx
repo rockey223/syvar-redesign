59e829af09059b27962c25b58962a6bab03b5199
rockey223 <rockeym50@gmail.com>
2025-03-04 19:05:46 +0545

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,12 +11,12 @@ const Navbar = () => {
             <div></div>
             <div></div>
         </div>
-        <div className="relative z-[55] flex px-[60px] justify-between items-center h-full">
+        <div className="relative z-[55] flex px-[60px] max-sm:px-[20px]  justify-between items-center h-full">
             <div className="logo h-16 w-16">
                 <Image src={'/logo.svg'} width={100} height={100} alt='logo' className='h-full w-full'/>
             </div>
             <div className="contact-btn">
-                <button className='h-12 w-32 bg-slate-800 text-white rounded-full uppercase'>contact us</button>
+                <button className='h-12 w-32 bg-slate-800 text-white font-semibold rounded-full uppercase'>contact us</button>
                 </div> 
         </div>
     </div>
