71c589fe6b6ebabf49c499e3226cb401baa02abc
rockey223 <rockeym50@gmail.com>
2025-03-04 21:35:30 +0545

    done

--- src/components/sections/Process.jsx
+++ src/components/sections/Process.jsx
@@ -46,17 +46,14 @@ const processes = [
 ];
 
 const Process = () => {
-
   const processSection = useRef();
 
-  
-  
   useGSAP(() => {
     let mm = gsap.matchMedia();
-    
+
     mm.add("(min-width: 1024px)", () => {
       const listItem = gsap.utils.toArray(".testing");
-  
+
       gsap.to(".left", {
         scrollTrigger: {
           trigger: ".process_wrapper",
@@ -66,10 +63,8 @@ const Process = () => {
           scrub: true,
         },
       });
-  
+
       listItem.forEach((list, index) => {
-      
-        
         if (index < listItem.length - 1) {
           gsap
             .timeline({
@@ -91,36 +86,36 @@ const Process = () => {
             );
         }
       });
-  
+
       gsap.set(".headers", { yPercent: 0 });
     });
-    mm.add("(max-width : 1023px)",()=>{
-      
-      
-      const list = gsap.utils.toArray("#list p") 
-list.forEach((ist)=>{
+    mm.add("(max-width : 1023px)", () => {
+      const elements = gsap.utils
+        .toArray(".mpoint");
 
-  gsap.to(ist,{
-    backgroundPosition: "0%",
-    stagger: 1,
-    scrollTrigger:{
-      trigger:ist,
-      scrub: true,
-      start: "top center",
-      end: "bottom center",
-    }
-  })
-})
+      elements.forEach((el) => {
+        gsap.to(el, {
+          backgroundPosition: "0% 50%",
+          stagger: 1,
+          scrollTrigger: {
+            trigger: el,
+            scrub: true,
+            start: "top 60%",
+            end: "bottom center",
+          },
+        });
+        
+      });
+    });
 
-
-    })
-  
     return () => mm.revert(); // Clean up on unmount
-  }, []); 
+  }, []);
 
   return (
-    <section id="our_process" ref={processSection} 
-    // className="bg-[#f8f6f2]"
+    <section
+      id="our_process"
+      ref={processSection}
+      // className="bg-[#f8f6f2]"
     >
       <div className="process_wrapper  flex flex-col lg:flex-row lg:justify-between max-lg:px-[30px] xl:pb-0 2xl:pb-[100px] pb-[100px]">
         {/* Left Section */}
@@ -155,19 +150,19 @@ list.forEach((ist)=>{
             {processes.map((process, index) => (
               <React.Fragment key={process.title + index}>
                 <div
-                id="list"
+                  id="list"
                   key={process.title + index}
                   className="right-content lg:pt-[155px] 2xl:pt-[190px] lg:pb-[254px] relative "
                 >
                   {/* <div className="absolute w-[3px] bg-red-700 border-4 border-red-400 h-full"></div> */}
-                  <p className="m_heading lg:hidden text-xl md:text-3xl  mb-2 font-semibold">
+                  <div className="testing lg:ml-[10px] xl:ml-[60px] flex flex-col lg:gap-y-[32px] mb-[20px] relative ">
+                  <p className="mpoint m_heading lg:hidden text-xl md:text-3xl  mb-2 font-semibold">
                     {`0${index + 1} ${process.title}`}
                   </p>
-                  <div className="testing lg:ml-[10px] xl:ml-[60px] flex flex-col lg:gap-y-[32px] mb-[20px] relative ">
                     {process.points.map((point, index) => (
                       <p
                         key={point}
-                        className="text-lg lg:text-[24px] xl:leading-[20px] 2xl:leading-[20px] "
+                        className="mpoint text-lg lg:text-[24px] xl:leading-[20px] 2xl:leading-[20px] "
                       >
                         {point}
                       </p>
@@ -175,7 +170,7 @@ list.forEach((ist)=>{
                   </div>
                 </div>
                 <hr
-                  className={`Line lg:ml-[10px] xl:ml-[60px] ${
+                  className={`mpoint Line lg:ml-[10px] xl:ml-[60px] ${
                     index === processes.length - 1 ? "hidden" : ""
                   }`}
                 />
